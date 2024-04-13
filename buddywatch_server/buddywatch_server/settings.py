d5332e3d28213d939479ca674bfe641a9c090d9d
Nakkeez <markus.halminen@gmail.com>
2024-04-13 12:24:27 +0300

    Add URL for media files

--- buddywatch_server/buddywatch_server/settings.py
+++ buddywatch_server/buddywatch_server/settings.py
@@ -140,6 +140,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
