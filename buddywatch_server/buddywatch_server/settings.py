e53cd3449db513757db815d8e6f5d45dcff3d63b
Nakkeez <markus.halminen@gmail.com>
2024-04-21 19:36:44 +0300

    Support PostgreSQL database instead of default SQLite

--- buddywatch_server/buddywatch_server/settings.py
+++ buddywatch_server/buddywatch_server/settings.py
@@ -92,12 +92,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'buddywatch_server.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'buddywatch',
+        'USER': 'dbadmin',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
