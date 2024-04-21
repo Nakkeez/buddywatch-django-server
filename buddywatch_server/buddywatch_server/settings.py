f6c86ca556cd64f20c9d5912cf710429dd7b091f
Nakkeez <markus.halminen@gmail.com>
2024-04-21 12:58:21 +0300

    Instead of default secret use environment variable

--- buddywatch_server/buddywatch_server/settings.py
+++ buddywatch_server/buddywatch_server/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o&k+wj2*m^xv!j#@s9tt@muimnmp@ig$qask)*cr2z4$t4(566'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
