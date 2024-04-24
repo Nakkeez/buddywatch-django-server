13091cda7c32e0e114d1d7946b81fcc24026d671
Nakkeez <markus.halminen@gmail.com>
2024-04-24 10:53:07 +0300

    Create test cases and add test data

--- buddywatch_server/api/tests.py
+++ buddywatch_server/api/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework.utils import json
+from django.conf import settings
+import os
 
-# Create your tests here.
+
+class JWTClient(Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.token = None
+
+    def authenticate(self, username=None, password=None):
+        response = self.post('/api/token/', {'username': username, 'password': password})
+        if response.status_code == 200:
+            self.token = json.loads(response.content.decode('utf-8'))["access"]
+
+    def _base_environ(self, **request):
+        environ = super()._base_environ(**request)
+        if self.token:
+            environ['HTTP_AUTHORIZATION'] = f'Bearer {self.token}'
+        return environ
+
+
+class ApiTest(TestCase):
+    def setUp(self):
+        self.client = JWTClient()
+        self.user = User.objects.create_user(username='testuser', password='testing')
+        self.client.authenticate(username='testuser', password='testing')
+
+    def test_refresh_token(self):
+        response = self.client.post("/api/token/", data={'username': 'testuser', 'password': 'testing'})
+        self.assertEqual(response.status_code, 200, 'The token should be successfully returned.')
+
+        response_content = json.loads(response.content.decode('utf-8'))
+        refresh_token = response_content['refresh']
+
+        response = self.client.post('/api/token/refresh/', data={'refresh': refresh_token})
+        self.assertEqual(response.status_code, 200, 'The new access token should be successfully returned')
+
+    def test_predict_view(self):
+        test_image = os.path.join(settings.MEDIA_ROOT, 'testing', 'test_image.png')
+
+        with open(test_image, 'rb') as image_file:
+            image = SimpleUploadedFile('test_image.jpg', image_file.read(), content_type='image/jpeg')
+            response = self.client.post('/api/predict/', {'image': image})
+        self.assertEqual(response.status_code, 200, 'Response should be successfully returned')
+
+        response_content = json.loads(response.content.decode('utf-8'))
+        bbox = response_content['prediction']['bbox']
+        label = response_content['prediction']['confidence']
+
+        self.assertEqual(len(bbox), 4, 'Bounding box should have 4 coordinates')
+        self.assertTrue(label, 'Confidence should be returned')
+
+    def test_get_videos(self):
+        response = self.client.get('/api/videos/')
+        self.assertEqual(response.status_code, 200)
--- buddywatch_server/buddywatch_server/settings.py
+++ buddywatch_server/buddywatch_server/settings.py
@@ -162,5 +162,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
